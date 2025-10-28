a95f9fa86908a477e03edf96e8cbf9729cbc1f61
Diego Cardenas Mejia <juan.diego.cardenas@correounivalle.edu.co>
2025-10-27 19:28:28 -0500

    Added frontend template

---
+++ frontend/src/components/footer/FooterAuth.js
@@ -0,0 +1,100 @@
+/*eslint-disable*/
+import React from "react";
+import {
+  Flex,
+  Link,
+  List,
+  ListItem,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Footer() {
+  let textColor = useColorModeValue("black", "white");
+  let linkColor = useColorModeValue({ base: "white", lg: "gray.900" }, "white");
+  return (
+    <Flex
+      zIndex='3'
+      background={'gray.300'}
+      w={'100vw'}
+      flexDirection={{
+        base: "column",
+        lg: "row",
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start",
+      }}
+      justifyContent='space-between'
+      px={{ base: "20px", md: "10px" }}
+      pb='10px'>
+      <Text
+        color={textColor}
+        textAlign={{
+          base: "center",
+          xl: "start",
+        }}
+        mb={{ base: "20px", lg: "0px" }}>
+        {" "}
+        &copy; {1900 + new Date().getYear()}
+        <Text as='span' fontWeight='500' ms='4px'>
+          Horizon UI. All Rights Reserved. Made with love by
+          <Link
+            mx='3px'
+            color={textColor}
+            href='https://www.simmmple.com?ref=horizon-chakra-free'
+            target='_blank'
+            fontWeight='700'>
+            Simmmple!
+          </Link>
+        </Text>
+      </Text>
+      <List display='flex'>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            fontWeight='500'
+            color={linkColor}
+            href='mailto:hello@simmmple.com?ref=horizon-chakra-free'>
+            Support
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            fontWeight='500'
+            color={linkColor}
+            href='https://www.simmmple.com/licenses?ref=horizon-chakra-free'>
+            License
+          </Link>
+        </ListItem>
+        <ListItem
+          me={{
+            base: "20px",
+            md: "44px",
+          }}>
+          <Link
+            fontWeight='500'
+            color={linkColor}
+            href='https://simmmple.com/terms-of-service?ref=horizon-chakra-free'>
+            Terms of Use
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link
+            fontWeight='500'
+            color={linkColor}
+            href='https://www.blog.simmmple.com/?ref=horizon-chakra-free'>
+            Blog
+          </Link>
+        </ListItem>
+      </List>
+    </Flex>
+  );
+}
---
+++ frontend/src/layouts/auth/Default.js
@@ -0,0 +1,87 @@
+// Chakra imports
+import { Box, Flex, Icon, Text } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+import React from "react";
+import Footer from "components/footer/FooterAuth";
+import FixedPlugin from "components/fixedPlugin/FixedPlugin";
+// Custom components
+import { NavLink } from "react-router-dom";
+// Assets
+import { FaChevronLeft } from "react-icons/fa";
+
+function AuthIllustration(props) {
+  const { children, illustrationBackground } = props;
+  // Chakra color mode
+  return (
+    <Flex position='relative' h='full' w='100%'>
+      <Flex
+        h={{
+          sm: "initial",
+          md: "unset",
+          lg: "100vh",
+          xl: "97vh",
+        }}
+        w={'100vw'}
+        maxW={'100vw'}
+        //maxW={{ md: "100%", lg: "1313px" }}
+        pt={{ sm: "0px", md: "0px" }}
+        px={{ lg: "0px", xl: "0px" }}
+        ps={{ xl: "0px" }}
+        justifyContent='start'
+        direction='column'>
+        <NavLink
+          to='/admin'
+          style={() => ({
+            width: "fit-content",
+            marginTop: "40px",
+          })}>
+          <Flex
+            align='center'
+            ps={{ base: "25px", lg: "10px" }}
+            pt={{ lg: "10px", xl: "10px" }}
+            w='fit-content'>
+            <Icon
+              as={FaChevronLeft}
+              me='12px'
+              h='13px'
+              w='8px'
+              color='secondaryGray.600'
+            />
+            <Text ms='10px' fontSize='sm' color='secondaryGray.600'>
+              De vuelta al inicio!
+            </Text>
+          </Flex>
+        </NavLink>
+          {children}
+          <Box
+              display={{ base: "none", md: "block" }}
+              h='100%'
+              w={{ lg: "50vw", "2xl": "44vw" }}
+              position='absolute'
+              right='0px'>
+              <Flex
+                  bg={`url(${illustrationBackground})`}
+                  justify='center'
+                  align='end'
+                  w='100%'
+                  h='100%'
+                  minH='100vh'
+                  bgSize='cover'
+                  bgPosition='50%'
+                  position='absolute'
+                  borderBottomLeftRadius={{ lg: "200px", xl: "200px" }}></Flex>
+          </Box>
+        <Footer />
+      </Flex>
+      <FixedPlugin />
+    </Flex>
+  );
+}
+// PROPS
+
+AuthIllustration.propTypes = {
+  illustrationBackground: PropTypes.string,
+  image: PropTypes.any,
+};
+
+export default AuthIllustration;
